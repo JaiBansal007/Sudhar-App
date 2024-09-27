3fa4c9115042f67d3faf461937593073b518deaa
JaiBansal007 <jai.bansal.jai@gmail.com>
2024-09-27 11:56:40 +0530

    few changes

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -1,13 +1,25 @@
 "use client";
 
+import { auth } from '@/firebase/config';
+import { on } from 'events';
+import { onAuthStateChanged } from 'firebase/auth';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
-  
+  const [show, setShow] = useState(false);
   const toggleMenu = () => setIsOpen(!isOpen);
-
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setShow(false);
+      } else {
+        setShow(true);
+      }
+    }
+    );
+  },[]); 
   return (
     <div>
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
@@ -18,18 +30,17 @@ export default function Navbar() {
               <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               <img src="/5.png" alt="Sudhaar Logo" className="h-8 inline-block" />
               </span>
-
             </button>
           </Link>
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-            <Link href="/signin">
+            {show && (<Link href="/signin">
               <button
                 type="button"
                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 Sign in
               </button>
-            </Link>
+            </Link>)}
             <button
               type="button"
               onClick={toggleMenu}
