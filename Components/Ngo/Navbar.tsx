8f8b4675ff617e27de8d5a3298a2431b89da5624
Nayan Jindal <nayan11404@gmail.com>
2024-09-27 16:24:36 +0530

    Photo links

--- Components/Ngo/Navbar.tsx
+++ Components/Ngo/Navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
     <div>
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <Link href="/">
+          <Link href="/ngo/home">
             <button className="flex items-center space-x-1 rtl:space-x-reverse">
               <img src="/1.png" className="h-12" alt="Logo" />
               <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
