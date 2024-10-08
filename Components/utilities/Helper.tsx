8fa6a205dd7e0cca977db54684c06038e6da07ac
JaiBansal007 <jai.bansal.jai@gmail.com>
2024-10-09 01:36:13 +0530

    interface done

--- Components/utilities/Helper.tsx
+++ Components/utilities/Helper.tsx
@@ -2,7 +2,6 @@
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth ,db} from '@/firebase/config';
 import { getDoc ,doc, query, collection, getDocs, where} from 'firebase/firestore';
-import bcrypt from 'bcrypt';
 //interfaces
 interface UserData{
     id:string ;
@@ -58,7 +57,8 @@ interface UserOrder{
     voucherPrice:number;
     voucherName:string;
     time:Date;
-
+    lat:string;
+    long:string;
 }
 interface Complaints{
     userid:string;
@@ -83,13 +83,12 @@ interface Complaints{
 }
 interface DealersOrder{
     dealerid:string;
-    id:number;
+    id:string;
     voucherPrice:number;
     voucherName:string;
     time:Date;
 
 }
-
 interface Post{
     id:string;
     userid:string;
@@ -124,12 +123,17 @@ export const isUserExist= async (email: string): Promise<boolean> => {
     }
 }
 
-export const getUserData = async (email: string): Promise<UserData> => {
+export const getUserData = async (email: string): Promise<UserData[]> => {
     try{
         const q= query(collection(db, 'users'), where('email', '==', email));
         const querySnapshot = await getDocs(q);
-        const UserData = querySnapshot.docs.map(doc => ({
-            ...doc.data()
+        const UserData =  querySnapshot.docs.map(doc => ({
+          id:doc.id,
+          balance:doc.data().balance,
+          name:doc.data().name,
+          email: doc.data().email,
+          password:doc.data().password,
+          phone_number:doc.data().phone_number
         }));
         return UserData;
     }catch(error){
@@ -151,12 +155,20 @@ export const isDealerExist= async (email: string): Promise<boolean> => {
     }
 }
 
-export const getDealerData = async (email: string): Promise<DealerData> => {
+export const getDealerData = async (email: string): Promise<DealerData[]> => {
     try{
       const q= query(collection(db, 'dealers'), where('email', '==', email));
       const querySnapshot = await getDocs(q);
       const DealerData = querySnapshot.docs.map(doc => ({
-          ...doc.data()
+          id:doc.id,
+          email:doc.data().email,
+          password:doc.data().password,
+          balance:doc.data().balance,
+          address:doc.data().address,
+          pincode:doc.data().pincode,
+          lng:doc.data().lng,
+          lat:doc.data().lat,
+          phone_number:doc.data().phone_number
       }));
       return DealerData;
     }catch(error){
@@ -181,12 +193,15 @@ export const isMCDExist= async (email: string): Promise<boolean> => {
 
 }
 
-export const getMCDData = async (email: string): Promise<MCDData> => {
+export const getMCDData = async (email: string): Promise<MCDData[]> => {
    try{
     const q= query(collection(db, 'mcd'), where('email', '==', email));
     const querySnapshot = await getDocs(q);
     const MCDData = querySnapshot.docs.map(doc => ({
-        ...doc.data()
+        id:doc.id,
+        email:doc.data().email,
+        password:doc.data().password,
+        pincode:doc.data().pincode
     }));
     return MCDData;
    }catch(error){
@@ -204,7 +219,13 @@ export const getuserallorders=async(userid:string):Promise<UserOrder[]>=>{
         const q= query(collection(db, 'orders'), where('userid', '==', userid));
         const querySnapshot = await getDocs(q);
         const orders = querySnapshot.docs.map(doc => ({
-            ...doc.data()
+            userid:doc.data().userid,
+            id:doc.id,
+            voucherPrice:doc.data().voucherPrice,
+            voucherName:doc.data().voucherName,
+            time:doc.data().time,
+            lat:doc.data().lat,
+            long:doc.data().long
         }));
         return orders;
     }catch(error){
@@ -220,7 +241,11 @@ export const getdealerallorders = async (dealerid: string): Promise<DealersOrder
       const q = query(collection(db, 'dealer_orders'), where('dealerid', '==', dealerid));
       const querySnapshot = await getDocs(q);
       const orders = querySnapshot.docs.map(doc => ({
-        ...doc.data()
+        dealerid:doc.data().dealerid,
+        id:doc.id,
+        voucherPrice:doc.data().voucherPrice,
+        voucherName:doc.data().voucherName,
+        time:doc.data().time
       }));
       return orders;
     } catch (error) {
@@ -235,7 +260,17 @@ export const getallcomplaints=async(pincode:string):Promise<Complaints[]>=>{
         const q= query(collection(db, 'complaints'), where('pincode', '==', pincode));
         const querySnapshot = await getDocs(q);
         const complaints = querySnapshot.docs.map(doc => ({
-            ...doc.data()
+            userid:doc.data().userid,
+            id:doc.id,
+            title:doc.data().title,
+            description:doc.data().description,
+            imageurl:doc.data().imageurl,
+            lat:doc.data().lat,
+            lng:doc.data().lng,
+            pincode:doc.data().pincode,
+            location:doc.data().location,
+            status:doc.data().status,
+            time:doc.data().time
         }));
         return complaints;
     }catch(error){
@@ -251,7 +286,20 @@ export const getUsertradingdetails=async(userid:string):Promise<trading[]>=>{
         const q= query(collection(db, 'trading'), where('userid', '==', userid));
         const querySnapshot = await getDocs(q);
         const trading_details = querySnapshot.docs.map(doc => ({
-            ...doc.data()
+            id:doc.id,
+            userid:doc.data().userid,
+            dealerid:doc.data().dealerid,
+            title:doc.data().title,
+            description:doc.data().description,
+            price:doc.data().price,
+            quantity:doc.data().quantity,
+            pincode:doc.data().pincode,
+            address:doc.data().address,
+            district:doc.data().district,
+            state:doc.data().state,
+            createdAt:doc.data().createdAt,
+            status:doc.data().status
+            
         }));
         return trading_details;
     }catch(error){
@@ -265,7 +313,19 @@ export const getDealertradingdetails=async(dealerid:string):Promise<trading[]>=>
         const q= query(collection(db, 'trading'), where('dealerid', '==', dealerid));
         const querySnapshot = await getDocs(q);
         const trading_details = querySnapshot.docs.map(doc => ({
-            ...doc.data()
+            id:doc.id,
+            userid:doc.data().userid,
+            dealerid:doc.data().dealerid,
+            title:doc.data().title,
+            description:doc.data().description,
+            price:doc.data().price,
+            quantity:doc.data().quantity,
+            pincode:doc.data().pincode,
+            address:doc.data().address,
+            district:doc.data().district,
+            state:doc.data().state,
+            createdAt:doc.data().createdAt,
+            status:doc.data().status
         }));
         return trading_details;
     }catch(error){
@@ -280,7 +340,12 @@ export const getallposts=async():Promise<Post[]>=>{
         const q= query(collection(db, 'post'));
         const querySnapshot = await getDocs(q);
         const posts = querySnapshot.docs.map(doc => ({
-            ...doc.data()
+            id:doc.id,
+            userid:doc.data().userid,
+            title:doc.data().title,
+            description:doc.data().description,
+            imageurl:doc.data().imageurl,
+            createdAt:doc.data().createdAt
         }));
         return posts;
     }catch(error){
